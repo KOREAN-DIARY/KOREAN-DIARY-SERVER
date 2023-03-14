225f64eca2928657adcbb7ba5a56e556233993bc
chaerin <achrvvv@gmail.com>
2023-03-14 15:10:42 +0900

    initial setting

---
+++ src/main/java/com/finalproject/kdiary/KdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.kdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdiaryApplication.class, args);
+	}
+
+}
