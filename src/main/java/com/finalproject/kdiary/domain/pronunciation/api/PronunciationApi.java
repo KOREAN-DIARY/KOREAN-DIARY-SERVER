873ebd2b55fdc9529e8a1196f599552ddec9fbdd
chaerin <achrvvv@gmail.com>
2023-03-30 17:33:28 +0900

    feat: add static audio file route

--- src/main/java/com/finalproject/kdiary/domain/pronunciation/api/PronunciationApi.java
+++ src/main/java/com/finalproject/kdiary/domain/pronunciation/api/PronunciationApi.java
@@ -1,16 +1,89 @@
 package com.finalproject.kdiary.domain.pronunciation.api;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
 
 
 @RestController
 @RequestMapping(value = "/pronunciation")
 public class PronunciationApi {
     @GetMapping()
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
-        return String.format("Hello %s!",name);
+    public String hello() {
+        String openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/PronunciationKor";
+        String accessKey = "60c7675c-c96b-413a-a331-e323521d9387";    // 발급받은 API Key
+        String languageCode = "korean";     // 언어 코드
+        String script = "형제 중에서 맏이가 제일 힘든 것 같아요.";    // 평가 대본
+        String audioContents = null;
+
+        Gson gson = new Gson();
+
+        Map<String, Object> request = new HashMap<>();
+        Map<String, String> argument = new HashMap<>();
+
+        try {
+            ClassPathResource resource = new ClassPathResource("001_034.pcm");
+            Path path = Paths.get(resource.getURI());
+            byte[] audioBytes = Files.readAllBytes(path);
+            audioContents = Base64.getEncoder().encodeToString(audioBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        argument.put("language_code", languageCode);
+        argument.put("script", script);
+        argument.put("audio", audioContents);
+
+        request.put("argument", argument);
+
+        URL url;
+        Integer responseCode = null;
+        String responBody = null;
+        try {
+            url = new URL(openApiURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            con.setRequestProperty("Authorization", accessKey);
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(gson.toJson(request).getBytes("UTF-8"));
+            wr.flush();
+            wr.close();
+
+            responseCode = con.getResponseCode();
+            InputStream is = con.getInputStream();
+            byte[] buffer = new byte[is.available()];
+            int byteRead = is.read(buffer);
+            responBody = new String(buffer);
+
+            System.out.println("[responseCode] " + responseCode);
+            System.out.println("[responBody]");
+            System.out.println(responBody);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return responBody;
     }
 }
