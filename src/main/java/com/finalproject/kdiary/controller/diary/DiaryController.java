6e393b83a7ebe8a94f4086c187c2797983474c19
chaerin <achrvvv@gmail.com>
2023-05-09 01:23:40 +0900

    refactor: refactor folder structure

--- src/main/java/com/finalproject/kdiary/controller/diary/DiaryController.java
+++ src/main/java/com/finalproject/kdiary/controller/diary/DiaryController.java
@@ -3,7 +3,7 @@ package com.finalproject.kdiary.controller.diary;
 import com.finalproject.kdiary.controller.diary.dto.request.DiaryCreateRequestDto;
 import com.finalproject.kdiary.controller.diary.dto.response.DiaryCreateResponseDto;
 import com.finalproject.kdiary.service.DiaryService;
-import com.finalproject.kdiary.support.response.ApiResponse;
+import com.finalproject.kdiary.support.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,9 +18,9 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @PostMapping("/diary")
-    public ApiResponse<DiaryCreateResponseDto> create(@RequestBody @Valid final DiaryCreateRequestDto request) {
+    public Response<DiaryCreateResponseDto> create(@RequestBody @Valid final DiaryCreateRequestDto request) {
         // TODO: replace user id with real jwt token
-        String userId = "91b6bc99-45f2-4bad-a73c-ba8c8a4010f";
-        return ApiResponse.success(diaryService.create(userId, request));
+        String userId = "91b6bc99-45f2-4bad-a73c-ba8c8a4010f8";
+        return Response.success(diaryService.create(userId, request));
     }
 }
--- src/main/java/com/finalproject/kdiary/controller/user/UserController.java
+++ src/main/java/com/finalproject/kdiary/controller/user/UserController.java
@@ -3,7 +3,7 @@ package com.finalproject.kdiary.controller.user;
 import com.finalproject.kdiary.controller.user.dto.request.UserCreateRequestDto;
 import com.finalproject.kdiary.controller.user.dto.response.UserCreateResponseDto;
 import com.finalproject.kdiary.service.UserService;
-import com.finalproject.kdiary.support.response.ApiResponse;
+import com.finalproject.kdiary.support.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,8 +17,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public ApiResponse<UserCreateResponseDto> create(@RequestBody @Valid final UserCreateRequestDto request) {
-        return ApiResponse.success(userService.create(request));
+    public Response<UserCreateResponseDto> create(@RequestBody @Valid final UserCreateRequestDto request) {
+        return Response.success(userService.create(request));
 
     }
 }
