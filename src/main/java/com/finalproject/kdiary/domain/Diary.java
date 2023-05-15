7fc83232a7193e7bb912f460494bba57ddb62bf4
chaerin <achrvvv@gmail.com>
2023-05-15 17:44:18 +0900

    fix: fix diary content max length

--- src/main/java/com/finalproject/kdiary/domain/Diary.java
+++ src/main/java/com/finalproject/kdiary/domain/Diary.java
@@ -18,7 +18,7 @@ public class Diary {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 3000)
     private String content;
 
     @Temporal(TemporalType.DATE)
