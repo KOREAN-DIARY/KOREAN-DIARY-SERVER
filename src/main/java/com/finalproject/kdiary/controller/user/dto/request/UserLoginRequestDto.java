881ba53bcaaf557babccb6427bf8d09b701b0efe
chaerin <achrvvv@gmail.com>
2023-08-11 10:11:50 -0700

    feat: integrate signin and signup

--- src/main/java/com/finalproject/kdiary/controller/user/dto/request/UserLoginRequestDto.java
+++ src/main/java/com/finalproject/kdiary/controller/user/dto/request/UserLoginRequestDto.java
@@ -3,6 +3,7 @@ package com.finalproject.kdiary.controller.user.dto.request;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -10,17 +11,13 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class UserLoginRequestDto {
-
     @Email(message = "이메일 형식에 맞지 않습니다")
-    @NotBlank
-    private String email;
-
     @NotNull
-    @Pattern(
-            regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다"
-    )
-    private String password;
+    private final String email;
+
+    @NotBlank
+    @Pattern(regexp = "^[가-힣a-zA-Z]{2,10}$", message = "이름을 입력해주세요.")
+    private final String name;
 }
