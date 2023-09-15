b051338d33684e1738c80e67e17e8147dfd5e23c
SoleiI <kmssun0524@sookmyung.ac.kr>
2023-09-15 23:36:49 +0900

    fix: fix order of response data in chart api

--- src/main/java/com/finalproject/kdiary/service/ChartService.java
+++ src/main/java/com/finalproject/kdiary/service/ChartService.java
@@ -41,9 +41,9 @@ public class ChartService {
 
         /* chart response data */
         List<ChartDto> chartList = new ArrayList<>();
-        chartList.add(ChartDto.of("말하기", "hsl(102, 70%, 50%)", speakingScores));
-        chartList.add(ChartDto.of("쓰기", "hsl(225, 70%, 50%)", writingScores));
         chartList.add(ChartDto.of("총점", "hsl(152, 70%, 50%)", totalScores));
+        chartList.add(ChartDto.of("쓰기", "hsl(225, 70%, 50%)", writingScores));
+        chartList.add(ChartDto.of("말하기", "hsl(102, 70%, 50%)", speakingScores));
 
         return chartList;
     }
